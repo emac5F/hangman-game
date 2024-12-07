10137e65060ae00c561473b5d22b74bd7679b8cf
emac5F <emac109508@gmail.com>
2024-12-07 19:17:30 -0400

    my first commit

---
+++ hanging/src/App.tsx
@@ -0,0 +1,33 @@
+import { letters } from './helpers/letters';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      {/* Imagenes */}
+      <h3>Imagen del juego</h3>
+
+      {/* Palabra oculta */}
+      <h3>_ _ _ _ _ _ _ _ _ _</h3>
+
+      {/* Contador de intentos */}
+      <h3>Intentos: 0</h3>
+
+      {/* Botones de letras */}
+      {
+        letters.map( (letter) => (
+          <button 
+            key={ letter }>
+              { letter }
+          </button>
+        )
+      )}
+
+    </div>
+  )
+
+}
+
+export default App
