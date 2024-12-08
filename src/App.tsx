20d7cbe8a891e56651154a7f36528ffcdb4562cb
emac5F <emac109508@gmail.com>
2024-12-07 21:19:23 -0400

    working with letter correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { HangImage } from './components/HangImage';
 import { letters } from './helpers/letters';
 
@@ -5,22 +6,41 @@ import './App.css'
 
 function App() {
 
+  //palabra oculta
+  const [ word ] = useState('COMPUTADORA');
+  const [ hiddenWord ] = useState( '_ '.repeat( word.length ) );
+
+  //intentos
+  const [ attempts, setAttempts ] = useState(0);
+
+  
+  const checkLetter = ( letter: string ) => {
+
+    if( !word.includes(letter) ){
+      setAttempts ( Math.min(attempts +1, 9) );
+      return;
+    } 
+    //console.log(letter + 'existe');
+    
+  }
+
   return (
     <div className='App'>
       
       {/* Imagenes */}
-      <HangImage imageNumber={9}/>
+      <HangImage imageNumber={attempts}/>
 
       {/* Palabra oculta */}
       <h3>_ _ _ _ _ _ _ _ _ _</h3>
 
       {/* Contador de intentos */}
-      <h3>Intentos: 0</h3>
+      <h3>Intentos: { attempts }</h3>
 
       {/* Botones de letras */}
       {
         letters.map( (letter) => (
           <button 
+            onClick={ () => checkLetter(letter) }
             key={ letter }>
               { letter }
           </button>
